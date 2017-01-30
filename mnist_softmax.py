483004bbd895467269488a563e1ed7c20c9c2b6d
Stefano Martina <stefano.martina@unifi.it>
2017-01-31 00:46:35 +0100

    finished softmax test

--- mnist_softmax.py
+++ mnist_softmax.py
@@ -32,3 +32,10 @@ for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
+#evaluating
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+#calculate accuracy from mean of 1:{correct_prediction == True}
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
